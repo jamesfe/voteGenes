20ce8206360ef9716fc9b4e1b67fa3f0b522c77f
JFerrara <james.ferrara@gmail.com>
2013-03-24 15:56:28 -0400

    Restructuring and testing of solutions.

--- adjacencyPoly.py
+++ adjacencyPoly.py
@@ -1,27 +1,76 @@
-import arcpy
+#import arcpy
 import pickle
 import random as r
 import sys
 import time
-
 r.seed(time.time())
 
 class precinct:
+    """a class to store demographic and graph information about precincts"""
     def __init__(self, a, b, precID, adjacent):
+        """initialization routine:
+        a - demographic (dems) 
+        b - demographic (reps)
+        precID - PREC_IDENT from shapefile
+        adjacent - list of adjacent PRECIDs"""
         self.a = a
         self.b = b
         self.precID = precID
         self.adj = adjacent
     def findValids(self, blackList):
+        """ findValids - find valid, adjacent precincts
+        blackList - set of non-valid precID values"""
         return(self.adj-blackList)
     def __repr__(self):
         return(str(self.precID)+" has "+str(self.a)+", "+str(self.a)+" and is adj to: "+str(self.adj))
 
-def printSol(inList):
-##    print inList
-    for k in range(0, len(inList)-1):
-        print '"PREC_IDENT" IN '+str([i.precID for i in inList[k]]).replace("[", "(").replace("]", ")")
-##            print '"PREC_IDENT" IN '+str([i.precID for i in solPool[sn]]).replace("[", "(").replace("]", ")")
+class fullSol:
+    def __init__(self):
+        self.sList = []
+    def addDistrict(self, dist):
+        self.sList.append(singleSol(dist))
+    def returnDistrictTotals(self):
+        """returnDistrictTotals - returns a balance of a's and b's
+            in short, if it returns x>0 there are x more a's than b's """
+        balanceTotal = 0
+        for k in self.sList:
+            a,b = k.calcABTotals()
+            if(a>b):
+                balanceTotal+=1
+            else:
+                balanceTotal-=1
+        return(balanceTotal)
+    def printQuery(self):
+        rt = ""
+        for sol in self.sList:
+            rt += sol.query()+"\n"
+        return(rt)
+    def popVariance(self):
+        totalPop = sum([i.population() for i in self.sList])
+        avgPop = (totalPop*1.0)/len(self.sList)
+        totalVar = sum([abs(avgPop-i.population()) for i in self.sList])/len(self.sList)
+        return(totalVar)
+ 
+class singleSol:
+    def __init__(self, precDataList):
+        self.PDL = precDataList
+        self.popVar = -1
+        self.totA = -1
+        self.totB = -1
+    def population(self):
+        return(sum([i.a+i.b for i in self.PDL]))
+    def calcABTotals(self):
+        self.totA = 0
+        self.totB = 0    
+        for k in self.PDL:
+            if(k.a>k.b):
+                self.totA+=1
+            else:
+                self.totB+=1
+        return(self.totA, self.totB)
+    def query(self):
+        qstr = '"PREC_IDENT" IN '+str([i.precID for i in self.PDL]).replace("[", "(").replace("]", ")")
+        return(qstr)
 
 def main(inShp):
 ##    featureDB, adjacencyDB = genAdjacency(inShp)
@@ -34,32 +83,48 @@ def main(inShp):
     print "Loading data..."
     featureDB = pickle.load(open('fDB.pickle', 'rb'))
     adjacencyDB = pickle.load(open('aDB.pickle', 'rb'))
-    ##tempSol = generateSolution(featureDB, adjacencyDB)
-    numSols = 10
+    print "Attempting solutions."
+    t = 5
+    s = fullSol() 
+    count = 0
+    while(t!=3):
+        count+=1
+        if(count>10000):
+            print "Out of time."
+            break
+        if(count%100==0):
+            print count,
+        t, s = genTestSol(featureDB, adjacencyDB)
+
+    print s.printQuery()
+    print s.returnDistrictTotals()
+    print s.popVariance()
+    """
+    numSols = 10000
     print("Calculating "+str(numSols)+" solutions...")
     solMat = [genSol2(featureDB, adjacencyDB) for i in range(0, numSols)]
-    for sol in range(0, len(solMat)):
-        dPop = []
-        solMD = []
-        for k in solMat[sol]:
-            totalPop = sum([r.a+r.b for r in k])
-            numA = sum([r.a for r in k])
-            numB = sum([r.b for r in k])
-            solMD.append([totalPop, numA, numB])
-            #print [totalPop, numA, numB],
-        a0 = (sum([i[0] for i in solMD])*1.0)/len(solMD)
-        a1 =  (sum([i[1] for i in solMD])*1.0)/len(solMD)
-        a2 = (sum([i[2] for i in solMD])*1.0)/len(solMD)
-        v0 = (sum([abs(i[0]-a0) for i in solMD])*1.0)/len(solMD)
-        v1 = (sum([abs(i[1]-a1) for i in solMD])*1.0)/len(solMD)
-        v2 = (sum([abs(i[2]-a2) for i in solMD])*1.0)/len(solMD)
-        solMat[sol].append([v0,a1,a2])
-        solMD = []
-    r = min([i[5][0] for i in solMat])
-    print "Min is: "+str(r)
-    for k in solMat:
-        if(k[5][0] == r):
-            printSol(k)
+    print("Solutions generated - moving on to evaluation...")
+    solutions = []
+    for sol in solMat:
+        t = fullSol()
+        for k in sol:
+            t.addDistrict(k)
+        solutions.append(t)
+   
+    for sol in solutions:
+        abTot = sol.returnDistrictTotals()
+        if(abTot==1):
+            print "AB Total: ",abTot
+            print sol.printQuery()
+            print "Variance: ",sol.popVariance()
+    """
+
+def genTestSol(fDB, aDB):
+    pSol = genSol2(fDB, aDB)
+    tester = fullSol()
+    for k in pSol:
+        tester.addDistrict(k)
+    return(tester.returnDistrictTotals(), tester)
 
 def genSol2(fDB, aDB):
     precPool = []
@@ -95,86 +160,8 @@ def genSol2(fDB, aDB):
                 precPool.remove(tgtPrec)
                 s.append(tgtPrec)
                 blackList.add(tgtPrec.precID)
-##    for sn in range(rSols):
-##        print '"PREC_IDENT" IN '+str([i.precID for i in solPool[sn]]).replace("[", "(").replace("]", ")")
     return(solPool)
-            
-def generateSolution(fDB, aDB):
-    reqParts = [(2, 1, 2), (1,2,3)] ## we want 1>2 3 times and 2>1 2 times
-    blackList = set()
-    validList = set()
-    currentSolution = set()
-    currentASum = 0
-    currentBSum = 0
-    totalSolution = []
-    for req in reqParts:
-        print "Req 2: ", req[2]
-        while(req[2]>0):
-            currentSolution = set()
-            blackList = set()
-            currentASum = 0
-            currentBSum = 0            
-            for i in range(0, len(totalSolution)):
-                blackList = blackList.union(set(totalSolution[i]))
-            currentASum = 0
-            currentBSum = 0
-            noSolution = True
-            while(len(currentSolution)==0):
-                k = fDB[r.randint(0, len(fDB)-1)]
-##                print req[0], req[1], k[req[0]], k[req[1]]
-                if(k[0] not in blackList) & (k[req[0]] > k[req[1]]):
-                    blackList.add(k[0])
-                    currentSolution.add(k[0])
-                    validList = validList.union(aDB[k[0]]) ## adjacency with this PREC_IDENT
-                    currentASum = k[req[0]]
-                    currentBSum = k[req[1]]
-            count = 0
-            print validList
-            while(True):
-                count+=1
-                if(count>10000):
-                    print "Forcibly Killing the program."
-                    sys.exit()
-                if(count>5000):
-                    noSolution = True
-                    print currentSolution
-                    
-                    print "Count Fail."
-                    count = 0
-                    break
-                if(len(validList)==0):
-                    if(currentASum+currentBSum>50000):
-                        noSolution = False
-                        print currentSolution
-                        print "Ding!"
-                        break
-                    else:
-                        noSolution = True
-                        print currentSolution                        
-                        print "ValidList Fail."
-                        break
-                pA = list(validList)[r.randint(0, len(validList)-1)]
-##                print "Proposed addition: ",pA
-                pA = fDB[[i for i in range(len(fDB)) if(fDB[i][0]==pA)][0]]
-                if(currentASum+pA[req[0]] > currentASum+pA[req[1]]):
-                    currentSolution.add(pA[0])
-##                    print "Adding ",pA[0]
-                    blackList.add(pA[0])
-                    currentASum+=pA[req[0]]
-                    currentBSum+=pA[req[1]]
-                    validList = set()
-                    for i in currentSolution:
-                        validList = validList.union(aDB[i])
-                    validList = validList-blackList
-##                print validList
-            if(not noSolution):
-                totalSolution.append([currentSolution])
-                req[2]-=1
-                print "Exit: ", totalSolution
-            print "out"
         
-    
-
 def genAdjacency(inShp):
     sCursor = arcpy.SearchCursor(inShp)
     featureDB = []
@@ -204,7 +191,6 @@ def genAdjacency(inShp):
                 adjacencyDB[featureDB[j][0]].append(featureDB[i][0])
 
     return(featureDB, adjacencyDB)
-    
 
 if(__name__=="__main__"):
     main('./FFXVotes_DissJoin.shp')
